13a7a893039cd3eb160ee7e7046fab16acddfe43
Bhavya Sharma <bhavyasharma4708@gmail.com>
2026-05-13 19:26:28 +0000

    number methods

---
+++ 01_basics/Nums_and_maths.js
@@ -0,0 +1,20 @@
+const score = 100;
+console.log(score);
+
+const value = new Number(100)
+console.log(value);
+
+console.log(value.toString());
+console.log(value.toFixed(2));   // 100.00
+
+const num = 123.83;
+console.log(num.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString(`en-IN`));  // 1,00,00,000 to indian format en-IN
+
+
+
+
+
+
