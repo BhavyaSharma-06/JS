081df26191a9bbc5080e2c93e826577afb5f07b8
Bhavya Sharma <bhavyasharma4708@gmail.com>
2026-05-13 08:46:42 +0000

    Conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = undefined;
+console.log(typeof score);
+let convTonum = Number(score);
+console.log(typeof convTonum );
+console.log(convTonum);
+
+/* Case study convert into number
+"33" = 33
+"33abc" = NaN
+true = 1
+false = 0
+*/
+
+let isLoggedIn = 1;
+console.log(typeof isLoggedIn);
+let convTobool = Boolean(isLoggedIn);
+console.log(typeof convTobool)
+console.log(convTobool);
+
+/* Case study convert into boolean
+1 = true
+0 = false
+"" = false
+"abc" = true
+*/
+
+let hinumber = 33;
+console.log(typeof hinumber)
+let convTostring = String(hinumber)
+console.log(typeof convTostring )
+console.log(convTostring);
+
+/* Case study convert into string
+33 = "33"
+*/
+
+
+
+
+
