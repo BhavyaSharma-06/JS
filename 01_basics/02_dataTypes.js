3f98fd9298ea1aa683ad4dc8ddb291328c872658
Bhavya Sharma <bhavyasharma4708@gmail.com>
2026-05-13 08:17:07 +0000

    learning about datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+/* number = 2 to power 53
+bigint = for big integer values
+string = ""
+boolean = true/false
+null = standalone values
+undefined = not yet defined
+symbol = uniquev major used in react
+
+object
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
